f162bd5b9bbbfa3dc8719e4876dfc815d5b371fc
mini11e <dmuth@uos.de>
2025-10-13 12:21:04 +0200

    testy

--- testy.py
+++ testy.py
@@ -5,19 +5,19 @@ import matplotlib.cbook as cbook
 import matplotlib.cm as cm
 from matplotlib.patches import PathPatch
 from matplotlib.path import Path
-
+if __name__ == "__main__":
 # Fixing random state for reproducibility
-np.random.seed(19680801)
-delta = 0.025
-x = y = np.arange(-3.0, 3.0, delta)
-X, Y = np.meshgrid(x, y)
-Z1 = np.exp(-X**2 - Y**2)
-Z2 = np.exp(-(X - 1)**2 - (Y - 1)**2)
-Z = (Z1 - Z2) * 2
+    np.random.seed(19680801)
+    delta = 0.025
+    x = y = np.arange(-3.0, 3.0, delta)
+    X, Y = np.meshgrid(x, y)
+    Z1 = np.exp(-X**2 - Y**2)
+    Z2 = np.exp(-(X - 1)**2 - (Y - 1)**2)
+    Z = (Z1 - Z2) * 2
 
-fig, ax = plt.subplots()
-im = ax.imshow(Z, interpolation='bilinear', cmap=cm.RdYlGn,
-               origin='lower', extent=[-3, 3, -3, 3],
-               vmax=abs(Z).max(), vmin=-abs(Z).max())
+    fig, ax = plt.subplots()
+    im = ax.imshow(Z, interpolation='bilinear', cmap=cm.RdYlGn,
+                origin='lower', extent=[-3, 3, -3, 3],
+                vmax=abs(Z).max(), vmin=-abs(Z).max())
 
-plt.show()
+    plt.show()
